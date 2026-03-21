9440cc090a0963bf9603167509b1c9e60918fbe5
HodynskyiYK <hodynskyiyk@gmail.com>
2026-03-21 23:02:28 +0100

    First setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,82 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
+import importPlugin from "eslint-plugin-import";
+import boundaries from "eslint-plugin-boundaries";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    ignores: ["dist", "build", "node_modules"],
   },
-])
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react": reactPlugin,
+      "react-hooks": hooksPlugin,
+      "import": importPlugin,
+      "boundaries": boundaries,
+    },
+    settings: {
+      react: { version: "detect" },
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+
+      "boundaries/include": ["src/**/*"],
+
+      "boundaries/elements": [
+        { type: "app", pattern: "src/app/**" },
+        { type: "entities", pattern: "src/entities/**" },
+        { type: "features", pattern: "src/features/**" },
+        { type: "pages", pattern: "src/pages/**" },
+        { type: "shared", pattern: "src/shared/**" },
+        { type: "widgets", pattern: "src/widgets/**" },
+      ],
+    },
+    rules: {
+      "boundaries/no-unknown": "error",
+      "boundaries/no-unknown-files": "error",
+
+      "boundaries/element-types": [
+        "error",
+        {
+          default: "disallow",
+          rules: [
+            { from: "app", allow: ["pages", "widgets", "features", "entities", "shared"] },
+            { from: "entities", allow: ["shared"] },
+            { from: "features", allow: ["entities", "shared"] },
+            { from: "pages", allow: ["widgets", "features", "entities", "shared"] },
+            { from: "shared", allow: ["shared"] },
+            { from: "widgets", allow: ["features", "entities", "shared"] },
+          ],
+        },
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          "patterns": [
+            {
+              "group": ["@/pages/*/*"],
+              "message": "Use public API (index.ts)"
+            },
+            {
+              "group": ["@/features/*/*"],
+              "message": "Use public API (index.ts)"
+            },
+            {
+              "group": ["@/entities/*/*"],
+              "message": "Use public API (index.ts)"
+            }
+          ]
+        }
+      ]
+    }
+  },
+  eslintConfigPrettier
+);
