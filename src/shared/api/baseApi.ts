4ff2cd50d811fb69cdaa87a01206d54f9d7dfd54
HodynskyiYK <hodynskyiyk@gmail.com>
2026-03-24 20:57:22 +0100

    Renamed reducerPath in baseApi

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 export const baseApi = createApi({
-  reducerPath: 'base',
+  reducerPath: 'api',
   tagTypes: ['User'],
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
